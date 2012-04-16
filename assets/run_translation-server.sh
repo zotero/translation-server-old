8e8a8ae9efa40515d3717cdf0a39136439aca889
Simon Kornblith <simon@simonster.com>
2012-04-15 23:50:07 -0400

    Load libraries properly

--- assets/run_translation-server.sh
+++ assets/run_translation-server.sh
@@ -2,4 +2,4 @@
 
 CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CALLDIR"
-./xpcshell -v 180 translation-server/init.js "$@"
+LD_LIBRARY_PATH="$CALLDIR:$LD_LIBRARY_PATH" ./xpcshell -v 180 translation-server/init.js "$@"
