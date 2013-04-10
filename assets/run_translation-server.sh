e866db92a18e1dfab71fac1547c8d00fcb984397
Simon Kornblith <simon@simonster.com>
2013-04-09 20:08:14 -0400

    Make Cygwin-compatible

--- assets/run_translation-server.sh
+++ assets/run_translation-server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
 CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CALLDIR"
-LD_LIBRARY_PATH="$CALLDIR:$LD_LIBRARY_PATH" ./xpcshell -v 180 -m -n translation-server/init.js "$@"
+
+if [[ "`uname`" == CYGWIN* ]]; then
+	./xpcshell.exe -v 180 -m -n translation-server/init.js "$@"
+else
+	LD_LIBRARY_PATH="$CALLDIR:$LD_LIBRARY_PATH" ./xpcshell -v 180 -m -n translation-server/init.js "$@"
+fi
