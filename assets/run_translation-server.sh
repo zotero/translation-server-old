7f5d8220799c48ae6ad6f1ccbf6223bcc0469617
Roan Kattouw <roan.kattouw@gmail.com>
2014-09-26 14:40:38 -0700

    Use xpcshell -mn , not -m -n
    -m -n breaks with the latest version of xpcshell.

--- assets/run_translation-server.sh
+++ assets/run_translation-server.sh
@@ -3,7 +3,7 @@ CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CALLDIR"
 
 if [[ "`uname`" == CYGWIN* ]]; then
-	./xpcshell.exe -v 180 -m -n translation-server/init.js "$@"
+	./xpcshell.exe -v 180 -mn translation-server/init.js "$@"
 else
-	LD_LIBRARY_PATH="$CALLDIR:$LD_LIBRARY_PATH" ./xpcshell -v 180 -m -n translation-server/init.js "$@"
+	LD_LIBRARY_PATH="$CALLDIR:$LD_LIBRARY_PATH" ./xpcshell -v 180 -mn translation-server/init.js "$@"
 fi
