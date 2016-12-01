bbc7c4045c6985d351b6152ef7b4a4d67b3b7735
Dan Stillman <dstillman@zotero.org>
2016-12-01 05:00:32 -0500

    Pull new translators on server start
    To skip, set SKIP_TRANSLATOR_UPDATE environmental variable to 1 (e.g., with
    `docker run -e SKIP_TRANSLATOR_UPDATE=1`).
    
    Closes #37

--- assets/run_translation-server.sh
+++ assets/run_translation-server.sh
@@ -2,6 +2,16 @@
 CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$CALLDIR"
 
+# Update translators to latest versions
+if [ "$SKIP_TRANSLATOR_UPDATE" != "1" ]; then
+	pushd "$CALLDIR/../modules/zotero/translators" > /dev/null
+	# Only update if a git repo
+	if [ -e .git ]; then
+		git pull origin master
+	fi
+	popd > /dev/null
+fi
+
 if [[ "`uname`" == CYGWIN* ]]; then
 	./xpcshell.exe -v 180 -mn translation-server/init.js "$@"
 else
